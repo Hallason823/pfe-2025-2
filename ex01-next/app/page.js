ba80f61857c9fc8483de4620b9be01fb252cbbcb
Hallason Matias <122497324+Hallason823@users.noreply.github.com>
2025-08-15 12:48:25 +0000

    feat: start react components

--- ex01-next/app/page.js
+++ ex01-next/app/page.js
@@ -1,8 +1,15 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+//import Image from "next/image";
+//import styles from "./page.module.css";
+
+import { Profile } from "@/components/Profile"
 
 export default function Home() {
   return (
-    <div>Olá Turma!</div>
+    <div>
+      <h1>Amazing scientists</h1>
+      <Profile />
+      <Profile />
+      <Profile />   
+    </div>
   );
 }
---
+++ ex01-next/components/Profile.js
@@ -0,0 +1,10 @@
+export function Profile() {
+    return (
+        <img 
+        src="https://i.imgur.com/MK3eW3As.jpg"
+        height={200}
+        wifth={200}
+        alt="Katherine Johnson"
+        />
+    );
+}
