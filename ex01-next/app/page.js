503d8d2d4609161cfec4608bee04a8a990bd4939
Hallason Matias <122497324+Hallason823@users.noreply.github.com>
2025-08-18 23:49:26 +0000

    Add the component TodoList

--- ex01-next/app/page.js
+++ ex01-next/app/page.js
@@ -2,6 +2,7 @@
 //import styles from "./page.module.css";
 
 import { Profile } from "@/components/Profile"
+import { TOdoList } from "@/components/TodoList";
 
 export default function Home() {
   return (
@@ -9,7 +10,8 @@ export default function Home() {
       <h1>Amazing scientists</h1>
       <Profile />
       <Profile />
-      <Profile />   
+      <Profile />
+      <TOdoList />  
     </div>
   );
 }
---
+++ ex01-next/components/TodoList.js
@@ -0,0 +1,17 @@
+export function TOdoList() {
+    return (
+        <>
+        <h1> Hedy Lamarr's Todos</h1>
+        <img
+        src="httpas://i.imgur.com/yXOvdOSs.jpg" 
+        alt="Hedy Lamarr"
+        className="photo"
+        />
+        <ul>
+            <li>Invent new traffic lights</li>
+            <li>Rehearse smart movies</li>
+            <li> Improve the spectrum technology</li>
+        </ul>
+        </>
+    );
+}
