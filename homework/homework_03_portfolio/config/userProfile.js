20971617d7b7a2f651c7b86288bbec34be92c5b9
Hallason Matias <122497324+Hallason823@users.noreply.github.com>
2025-11-06 13:18:30 +0000

    remove: remove project for resume

--- homework/homework_03_portfolio/config/userProfile.js
+++
@@ -1,8 +0,0 @@
-export const userProfile = {
-  name: 'Hallason Matias',
-  profession: 'Full Stack Developer & AI Engineer',
-  imageSrc: '/images/profile_photo.jpeg',
-  altText: 'Profile Picture',
-  downloadCvLink: '/files/hallason_matias_resume.pdf',
-  contactEmail: 'hms3@cin.ufpe.br'
-};
